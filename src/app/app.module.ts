c9c95507e4304529d7ca7049f7099d1428bd8834
ravikrushnaassetmonk <ravikrushnaassetmonk@gmail.com>
2022-08-25 13:24:26 +0530

    final-commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { TodoComponent } from './todo/todo.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    TodoComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  
+ }
---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { VirtualTimeScheduler } from 'rxjs';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+  
+  
+ todoList : string [] = [];
+ inputvalue: string = "";
+ removetodo(todo : string): void{
+  this.todoList = this.todoList.filter(value => value !== todo)
+ }
+ addtodo(){
+   if(this.inputvalue != ""){
+    this.todoList.push(this.inputvalue);
+    this.inputvalue = "";
+   } 
+   else{
+    alert("Please Enter Proper Value")
+   }
+
+ }
+
+  constructor() { }
+  
+  ngOnInit(): void {
+    this.todoList = ["Task 1","Task 2"];
+  }
+
+}
